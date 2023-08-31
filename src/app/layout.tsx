477456928497e3fa7bb2f8beffd6afe7cf0ed47d
Emil Nielsen <hej@emilnielsen.com>
2023-08-31 09:51:59 +0700

    Refactor GMT implementatioN

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,9 @@
 import "./globals.css";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import Analytics from "./Analytics";
+
+import { Suspense } from "react";
 
 export default function RootLayout({
   children,
@@ -9,25 +12,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <head>
-        <script
-          id="google-tag-manager"
-          dangerouslySetInnerHTML={{
-            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-NHQZRLS9');`,
-          }}
-        />
-      </head>
+      <head></head>
       <body id="root">
-        <noscript
-          dangerouslySetInnerHTML={{
-            __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NHQZRLS9"
-height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
-          }}
-        />
+        <Suspense>
+          <Analytics />
+        </Suspense>
         {children}
       </body>
     </html>
