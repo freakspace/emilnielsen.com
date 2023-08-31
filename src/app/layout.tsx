3aa691af0a839b5d8da5975c5808cd40ca4c8ad2
Emil Nielsen <hej@emilnielsen.com>
2023-08-31 10:45:30 +0700

    Move gtm script

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,8 @@
 import "./globals.css";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import Script from "next/script";
 import Analytics from "./Analytics";
-
-import { Suspense } from "react";
 
 export default function RootLayout({
   children,
@@ -13,7 +12,15 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <Analytics />
+        <Script id="gtm-script" strategy="afterInteractive">
+          {`
+    (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer', 'GTM-NHQZRLS9');
+  `}
+        </Script>
       </head>
       <body id="root">{children}</body>
     </html>
